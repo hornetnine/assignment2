97253207265dcb67686fe07a9a51d07152123deb
yisiox <yisiong.y22@gmail.com>
2023-08-24 12:38:26 +0800

    update changes

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='h9',
+    maintainer='hornetnine',
     maintainer_email='hornetnine@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
